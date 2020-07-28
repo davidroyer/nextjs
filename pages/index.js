684b354a015931eb44309bc525553635d754b795
David Royer <droyer01@gmail.com>
2020-07-28 06:53:12 -0400

    chore(testing)

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+         David Royer - <a href="https://nextjs.org">Next.js Project</a>
       </h1>
 
       <p className="description">
